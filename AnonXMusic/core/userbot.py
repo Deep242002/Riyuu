de20fb064dbf7267ccb957355c6ea80aa80ae3b0
Deep242002 <kamal24zone@gmail.com>
2024-09-21 05:08:28 +0530

    Update userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -72,6 +72,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
+                await self.two.join_chat("nova_updats")
                 await self.two.join_chat("the_losthope")
                 await self.one.join_chat("where_lucy")
             except:
@@ -93,7 +94,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("the_losthope")
+                await self.two.join_chat("nova_updats")
+                await self.two.join_chat("the_losthope")
                 await self.one.join_chat("where_lucy")
             except:
                 pass
@@ -114,7 +116,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("the_losthope")
+                await self.two.join_chat("nova_updats")
+                await self.two.join_chat("the_losthope")
                 await self.one.join_chat("where_lucy")
             except:
                 pass
@@ -135,7 +138,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("the_losthope")
+                await self.two.join_chat("nova_updats")
+                await self.two.join_chat("the_losthope")
                 await self.one.join_chat("where_lucy")
             except:
                 pass
